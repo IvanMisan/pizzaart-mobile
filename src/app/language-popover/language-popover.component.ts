7de55e455cf8abc273d72a7ea31ef627d4c417ea
787 <mac787@787s-MacBook-Air.local>
2025-07-31 18:02:51 +0200

    Добавил новую фичу

---
+++ src/app/language-popover/language-popover.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';  // для *ngFor
+import { IonicModule } from '@ionic/angular';  // для ion-list, ion-item
+import { PopoverController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-language-popover',
+  templateUrl: './language-popover.component.html',
+  styleUrls: ['./language-popover.component.scss'],
+  standalone: true,
+  imports: [CommonModule, IonicModule], 
+})
+export class LanguagePopoverComponent {
+  languages = [
+    { code: 'en', label: 'English', flag: 'assets/flags/en.png' },
+    { code: 'pl', label: 'Polski', flag: 'assets/flags/pl.png' },
+    { code: 'ua', label: 'Українська', flag: 'assets/flags/ua.png' },
+  ];
+
+  constructor(private popoverCtrl: PopoverController) {}
+
+  selectLang(langCode: string) {
+    this.popoverCtrl.dismiss(langCode);
+  }
+}
