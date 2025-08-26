0385fa697eb36e4046eeda52f689d87ff88346ae
Ivan Misan <101949899+IvanMisan@users.noreply.github.com>
2025-08-26 09:06:53 +0200

    Add hamburger Manu (#6)
    * fix build
    
    * feat: add hamburger menu with navigation functionality
    
    * add and update hamburger menu
    
    * refactor: apply code review changes
    
    - Wrap cart modal styles in :host selector
    - Rename CartModalPage to CartModal
    - Move dependencies to constructor injection
    - Remove console.log from checkout method
    - Format ThemeService constructor
    
    ---------
    
    Co-authored-by: 787 <mac787@787s-MacBook-Air.local>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,20 @@
-import { NgModule, isDevMode } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { RouteReuseStrategy } from '@angular/router';
+import { isDevMode } from '@angular/core';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
-  declarations: [AppComponent],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
@@ -25,11 +22,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     HttpClientModule,
     TranslateModule.forRoot({
       defaultLanguage: 'ua',
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
+      loader: { provide: TranslateLoader, useFactory: HttpLoaderFactory, deps: [HttpClient] }
     }),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
@@ -38,5 +31,6 @@ export function HttpLoaderFactory(http: HttpClient) {
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] 
 })
 export class AppModule {}
